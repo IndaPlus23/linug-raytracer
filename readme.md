8f7987721dc0a401f17572076c84b255eec5d776
cl <cl.gustafsson@icloud.com>
2024-01-19 09:11:51 +0100

    Final scene

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Raytracer
+
+![Render](render.png "Render")
+
+Ray Tracing in One Weekend with added lights
+
+## How to run
+
+```zsh
+./raycasting
+```
+
+or
+
+```zsh
+cargo build --release
+```
+#### Note: --release flag is important for good performance
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
 use ::image as im;
-use piston_window::*;
-use cgmath::{dot, Vector2, Vector3, Matrix2, Rad, Deg, InnerSpace};
-use fps_counter::FPSCounter;
+use cgmath::{dot, Vector3, InnerSpace, ElementWise};
 use rand::Rng;
 
 const WINDOW_WIDTH: u32 = 800;
-const WINDOW_HEIGHT: u32 = 400;
+const WINDOW_HEIGHT: u32 = 450;
+
+const VFOV: f64 = 20.;
+const LOOKFROM: Vector3<f64> = Vector3 {x: 13., y: 2., z: 3.};
+const LOOKAT: Vector3<f64> = Vector3 {x: 0., y: 0., z: 0.};
+const VUP: Vector3<f64> = Vector3 {x: 0., y: 1., z: 0.};
+const DEFOCUS_ANGLE: f64 = 0.;
+const FOCUS_DIST: f64 = 10.;
+
+//const VFOV: f64 = 40.;
+//const LOOKFROM: Vector3<f64> = Vector3 {x: 800., y: 0., z: 0.};
+//const LOOKAT: Vector3<f64> = Vector3 {x: 0., y: 0., z: 0.};
+//const VUP: Vector3<f64> = Vector3 {x: 0., y: 1., z: 0.};
+//const DEFOCUS_ANGLE: f64 = 0.;
+//const FOCUS_DIST: f64 = 10.;
+const BACKGROUND: bool = false;
+
 
 const WINDOW_FWIDTH: f64 = WINDOW_WIDTH as f64;
 const WINDOW_FHEIGTH: f64 = WINDOW_HEIGHT as f64;
 
 const PI: f64 = 3.1415926535897932385;
+
+fn degree_to_radians(degrees: f64) -> f64 {
+    degrees * PI / 180.
+}
 
 fn random_0_1() -> f64 {
     return rand::thread_rng().gen_range(0.0..1.0)
@@ -20,6 +38,65 @@ fn random_in_interval(i: Interval) -> f64 {
     return rand::thread_rng().gen_range(i.min..i.max)
 }
 
+fn random_vec() -> Vector3<f64>{
+    Vector3::from([random_0_1(), random_0_1(), random_0_1()])
+}
+
+fn random_vec_from_interval(i: Interval) -> Vector3<f64> {
+    Vector3::from([random_in_interval(i), random_in_interval(i), random_in_interval(i)])
+}
+
+fn random_vec_in_unit_sphere() -> Vector3<f64> {
+    loop {
+        let p = random_vec_from_interval(Interval::from(-1., 1.));
+        if p.magnitude2() < 1. {return p}
+    }
+}
+
+fn random_unit_vec() -> Vector3<f64> {
+    random_vec_in_unit_sphere().normalize()
+}
+
+fn random_on_hemisphere(normal: Vector3<f64>) -> Vector3<f64>{
+    let on_unit_sphere = random_unit_vec();
+    if dot(on_unit_sphere, normal) > 0. {
+        on_unit_sphere
+    }
+    else {
+        -on_unit_sphere
+    }
+}
+
+fn random_in_unit_disk() -> Vector3<f64> {
+    loop {
+        let p = Vector3::from([random_in_interval(Interval::from(-1., 1.)),random_in_interval(Interval::from(-1., 1.)),0.]);
+        if p.magnitude2() < 1. {return p}
+    }
+}
+
+fn linear_to_gamma(linear_component: f64) -> f64 {
+    linear_component.sqrt()
+}
+
+fn reflect(v: Vector3<f64>, normal: Vector3<f64>) -> Vector3<f64> {
+    v - 2. * dot(v, normal) * normal
+}
+
+fn refract(uv: Vector3<f64>, normal: Vector3<f64>, etai_over_etat: f64) -> Vector3<f64> {
+    let cos_theta = f64::min(dot(-uv, normal), 1.);
+    let r_out_perp = etai_over_etat * (uv + cos_theta*normal);
+    let r_out_parallel = -(1.0-r_out_perp.magnitude2()).abs().sqrt() * normal;
+    r_out_perp + r_out_parallel
+}
+
+fn reflectance(cosine: f64, relection_i: f64) -> f64{
+    //Schlicks approximation
+    let mut r0 = (1.-relection_i) / (1.+relection_i);
+    r0 = r0*r0;
+    r0 + (1.-r0)*(1.-cosine)*(1.-cosine)*(1.-cosine)*(1.-cosine)*(1.-cosine)
+}
+
+#[derive(Clone, Copy)]
 struct Interval {
     min: f64,
     max: f64
@@ -52,18 +129,99 @@ const EMPTY: Interval = Interval {min: f64::INFINITY, max: f64::NEG_INFINITY};
 
 const UNIVERSE: Interval = Interval {min: f64::NEG_INFINITY, max: f64::INFINITY};
 
+fn rgb_to_color(mut r: f64, mut g: f64, mut b: f64, samples: i32) -> [u8; 4] {
+    let scale = 1. / samples as f64;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
+
+    let intensity = Interval::from(0., 0.999);
+    [(255.99 * intensity.clamp(r)) as u8, (255.99 * intensity.clamp(g)) as u8, (255.99 * intensity.clamp(b)) as u8, 255]
+}
+
+#[derive(Clone, Copy)]
+enum MaterialType<Color, Fuzz, RefractionIndex> {
+    Lambertian(Color),
+    Metal(Color, Fuzz),
+    Dielectric(RefractionIndex),
+    DiffuseLight,
+    None
+}
+
+#[derive(Clone, Copy)]
+struct Material {
+    material_type: MaterialType<Vector3<f64>, f64, f64>,
+}
+impl Material {
+    fn scatter(&self, ray: Ray, rec: HitRecord, attenuation: &mut Vector3<f64>, scattered: &mut Ray) -> bool{
+        match self.material_type {
+            MaterialType::Lambertian(color) => {
+                let mut scatter_direction = rec.normal + random_unit_vec();
+                if near_zero(scatter_direction) {
+                    scatter_direction = rec.normal;
+                }
+                *scattered = Ray::new(rec.position, scatter_direction);
+                *attenuation = color;
+                true
+            }
+            MaterialType::Metal(color, fuzz) => {
+                let reflected = reflect(ray.direction.normalize(), rec.normal);
+                *scattered = Ray::new(rec.position, reflected + fuzz * random_unit_vec());
+                *attenuation = color;
+                dot(scattered.direction, rec.normal) > 0.
+            }
+            MaterialType::Dielectric(ir) => {
+                *attenuation = Vector3::from([1.,1.,1.]);
+                let refraction_ratio = if rec.front_face {1. / ir}  else {ir};
+                let unit_direction = ray.direction.normalize();
+
+                let cos_theta = f64::min(dot(-unit_direction, rec.normal), 1.);
+                let sin_theta = (1. - cos_theta*cos_theta).sqrt();
+
+                let cannot_refract = refraction_ratio * sin_theta > 1.;
+                let direction;
+
+                if cannot_refract || reflectance(cos_theta, refraction_ratio) > random_0_1() 
+                {direction = reflect(unit_direction, rec.normal)} else 
+                {direction = refract(unit_direction, rec.normal, refraction_ratio)}
+
+                *scattered = Ray::new(rec.position, direction);
+                true
+            },
+            MaterialType::DiffuseLight => {false}
+            _ => {true}
+        }
+    }
+
+    fn emmited(&self) -> Vector3<f64> {
+        match self.material_type {
+            MaterialType::DiffuseLight => Vector3::from([1.,1.,1.]),
+            _ => Vector3::from([0.,0.,0.])
+        }
+    }
+}
+
+fn near_zero(v: Vector3<f64>) -> bool {
+    let limit = 1e-8;
+    v.x < limit && v.y < limit && v.z < limit
+}
 
 #[derive(Clone, Copy)]
 struct HitRecord {
     position: Vector3<f64>,
     normal: Vector3<f64>,
+    material: Material,
     t: f64,
     front_face: bool
 }
 
 impl HitRecord {
     fn new() -> HitRecord {
-        HitRecord {position: Vector3::from([0.,0.,0.]), normal: Vector3::from([0.,0.,0.]), t: 0., front_face: true}
+        HitRecord {position: Vector3::from([0.,0.,0.]), normal: Vector3::from([0.,0.,0.]), material: Material {material_type: MaterialType::None}, t: 0., front_face: true}
     }
 
     fn set_face_normal(&mut self, r: Ray, outward_normal: Vector3<f64>) {
@@ -74,7 +232,8 @@ impl HitRecord {
 #[derive(Clone)]
 struct Sphere {
     position: Vector3<f64>,
-    radius: f64
+    radius: f64,
+    material: Material
 }
 #[derive(Clone)]
 struct World {
@@ -92,7 +251,7 @@ impl World {
         let mut closest_so_far = ray_t.max;
 
         for sphere in &self.spheres {
-            if r.hit_sphere(sphere.position, sphere.radius, Interval::from(ray_t.min, closest_so_far), &mut temp_rec) {
+            if r.hit_sphere(sphere, Interval::from(ray_t.min, closest_so_far), &mut temp_rec) {
                 hit_anything = true;
                 closest_so_far = temp_rec.t;
                 *rec = temp_rec;
@@ -115,11 +274,11 @@ impl Ray {
    fn at(&self, t: f64) -> Vector3<f64> {
         self.origin + t * self.direction
    }
-   fn hit_sphere(&self, sphere_center: Vector3<f64>, sphere_radius: f64, ray_t: Interval, rec: &mut HitRecord) -> bool {
-        let oc = self.origin - sphere_center;
+   fn hit_sphere(&self, sphere: &Sphere, ray_t: Interval, rec: &mut HitRecord) -> bool {
+        let oc = self.origin - sphere.position;
         let a: f64 = self.direction.magnitude2();
         let half_b = dot(oc, self.direction);
-        let c = oc.magnitude2() - sphere_radius * sphere_radius;
+        let c = oc.magnitude2() - sphere.radius * sphere.radius;
         let discriminant = half_b*half_b -a*c;
         if discriminant < 0. {
             return false
@@ -134,30 +293,16 @@ impl Ray {
         }
         rec.t = root;
         rec.position = self.at(rec.t);
-        rec.normal = (rec.position - sphere_center) / sphere_radius;
-        let outward_normal = (rec.position - sphere_center) / sphere_radius;
+        rec.normal = (rec.position - sphere.position) / sphere.radius;
+        let outward_normal = (rec.position - sphere.position) / sphere.radius;
         rec.set_face_normal(*self, outward_normal);
+        rec.material = sphere.material;
 
         true
 
    }
 
-   fn ray_color(&self, world: World) -> [u8; 4] {
-        let mut rec = HitRecord::new();
-        if world.hit_spheres(*self, Interval::from(0., f64::INFINITY) , &mut rec) {
-            let r = (0.5 * (rec.normal.x+1.))*255.99;
-            let g = (0.5 * (rec.normal.y+1.))*255.99;
-            let b = (0.5 * (rec.normal.z+1.))*255.99;
-            return [r as u8, g as u8, b as u8, 255]
-        }
-        let a = (self.direction.normalize().y + 1.) / 2.;
-        let r = ((1.-a) * 1. + a * 0.5)*255.99;
-        let g = ((1.-a) * 1. + a * 0.7)*255.99;
-        let b = ((1.-a) * 1. + a * 1.)*255.99;
-        [r as u8, g as u8, b as u8, 255]
-
-
-   }
+ 
    //fn color(&self) -> [u8; 4] {
    //     let t = self.hit_sphere(Vector3::from([0., 0., -1.]), 0.5);
    //     if t > 0. {
@@ -175,83 +320,157 @@ impl Ray {
    //     [r as u8, g as u8, b as u8, 255]
    //}
 }
+fn ray_color(r: Ray, depth: i32, world: World) -> Vector3<f64> {
+    let mut rec = HitRecord::new();
+    if depth <= 0 {return Vector3::from([0.,0.,0.])}
 
-fn render(canvas: &mut im::ImageBuffer<im::Rgba<u8>, Vec<u8>>, camera_position: Vector3<f64>, world: World) {
+    if world.hit_spheres(r, Interval::from(0.001, f64::INFINITY) , &mut rec) {
+        let mut scattered = r;
+        let mut attenuation = Vector3::from([0.,0.,0.]);
+        let color_from_emission = rec.material.emmited();
+        if !rec.material.scatter(r, rec, &mut attenuation, &mut scattered) {
+            return color_from_emission;
+        }
+        let color_from_scatter = attenuation.mul_element_wise(ray_color(scattered, depth-1, world));
+        return color_from_emission + color_from_scatter;
+    }
+    else {
+        if BACKGROUND {
+            let unit_direction = r.direction.normalize();
+            let a = 0.5 * (unit_direction.y + 1.);
+            (1.-a)*Vector3::from([1.,1.,1.]) + a*Vector3::from([0.5,0.7,1.])
+        }
+        else {
+            Vector3::from([0.,0.,0.])
+        }
+    }
+   
 
-    let focal_length: f64 = 1.;
-    let viewport_width = 4.;
-    let viewport_height: f64 = 2.;
 
-    let viewport_u: Vector3<f64> = Vector3::from([viewport_width, 0., 0.]);
-    let viewport_v: Vector3<f64> = Vector3::from([0., -viewport_height, 0.]);
+}
+
+
+
+fn render(canvas: &mut im::ImageBuffer<im::Rgba<u8>, Vec<u8>>, world: World, samples_per_pixel: i32, max_depth: i32) {
+
+    let camera_position = LOOKFROM;
+
+    let vfov = VFOV;
+
+    let theta = degree_to_radians(vfov);
+    let h = (theta / 2.).tan();
+    let viewport_height: f64 = 2. * h * FOCUS_DIST;
+    let viewport_width = viewport_height * WINDOW_FWIDTH / WINDOW_FHEIGTH;
+
+    let w = (LOOKFROM-LOOKAT).normalize();
+    let u = VUP.cross(w).normalize();
+    let v = w.cross(u);
+
+    let viewport_u: Vector3<f64> = viewport_width * u;
+    let viewport_v: Vector3<f64> = viewport_height * -v;
 
     let pixel_delta_u = viewport_u / WINDOW_FWIDTH;
     let pixel_delta_v = viewport_v / WINDOW_FHEIGTH;
 
-    let viewport_top_left = camera_position - Vector3::from([0., 0., focal_length]) - viewport_u/2. - viewport_v/2.;
+    let viewport_top_left = camera_position - (FOCUS_DIST * w) - viewport_u/2. - viewport_v/2.;
     let pixel00_loc = viewport_top_left + (pixel_delta_u + pixel_delta_v) / 2.;
 
+    let defocus_radius = FOCUS_DIST * (degree_to_radians(DEFOCUS_ANGLE / 2.).tan());
+    let defocus_disk_u = u * defocus_radius;
+    let defocus_disk_v = v * defocus_radius;
 
     for x in 0..WINDOW_WIDTH {
         for y in 0..WINDOW_HEIGHT {
-            let pixel_center = pixel00_loc + pixel_delta_u * x as f64 + pixel_delta_v * y as f64;
-            let ray_direction = pixel_center - camera_position;
-
-            let ray = Ray::new(camera_position, ray_direction);
-            let r = x as f32 / (WINDOW_WIDTH as f32-1.)*255.99;
-            let g = y as f32 / (WINDOW_HEIGHT as f32-1.)*255.99;
-            let b = 0;
-            canvas.put_pixel(x, y, im::Rgba(ray.ray_color(world.clone())));
+            let mut pixel_color = Vector3::from([0.,0.,0.]);
+            for _ in 0..samples_per_pixel {
+                let r = get_ray(x, y, camera_position, pixel00_loc, pixel_delta_u, pixel_delta_v, defocus_disk_u, defocus_disk_v);
+                
+                //dbg!(pixel_color, sample_color);
+                pixel_color += ray_color(r, max_depth, world.clone());
+            }
+            let color = rgb_to_color(pixel_color.x, pixel_color.y, pixel_color.z, samples_per_pixel);
+            canvas.put_pixel(x, y, im::Rgba(color));
         }
     }          
 }
 
+fn get_ray(x: u32, y: u32, camera_position: Vector3<f64>, pixel00_loc : Vector3<f64>, pixel_delta_u: Vector3<f64>,
+     pixel_delta_v: Vector3<f64>, defocus_disk_u: Vector3<f64>, defocus_disk_v: Vector3<f64>) -> Ray {
+    let pixel_center = pixel00_loc + pixel_delta_u * x as f64 + pixel_delta_v * y as f64;
+    let pixel_sample = pixel_center + pixel_sample_square(pixel_delta_u, pixel_delta_v);
+    
+    let ray_origin = if DEFOCUS_ANGLE <= 0. {camera_position} else {defocus_disk_sample(camera_position, defocus_disk_u, defocus_disk_v)};
+    let ray_direction = pixel_sample - ray_origin;
+
+    Ray::new(camera_position, ray_direction)
+}
+
+fn defocus_disk_sample(position: Vector3<f64>, defocus_disk_u: Vector3<f64>, defocus_disk_v: Vector3<f64>) -> Vector3<f64> {
+    let p = random_in_unit_disk();
+    position + (p.x * defocus_disk_u) + (p.y * defocus_disk_v)
+}
+
+fn pixel_sample_square(pixel_delta_u: Vector3<f64>, pixel_delta_v: Vector3<f64>) -> Vector3<f64> {
+    let px = -0.5 + random_0_1();
+    let py = -0.5 + random_0_1();
+    (px * pixel_delta_u) + (py * pixel_delta_v)
+}
 
 fn main() {
-    let opengl = OpenGL::V3_2;
-    let mut window: PistonWindow =
-        WindowSettings::new("", (WINDOW_WIDTH, WINDOW_HEIGHT))
-        .exit_on_esc(true)
-        .graphics_api(opengl)
-        .build()
-        .unwrap();
-
-    let mut fps_counter = FPSCounter::new();
 
     let mut canvas = im::ImageBuffer::new(WINDOW_WIDTH, WINDOW_HEIGHT);
-    let mut texture_context = TextureContext {
-        factory: window.factory.clone(),
-        encoder: window.factory.create_command_buffer().into()
-    };
-    let mut texture: G2dTexture = Texture::from_image(
-            &mut texture_context,
-            &canvas,
-            &TextureSettings::new()
-        ).unwrap();
 
-
-    let camera_position: Vector3<f64> = Vector3::from([0., 0., 0.]);
+    //making the scene
     let mut world = World::new();
-    world.spheres.push(Sphere { position: Vector3::from([0., 0., -1.]), radius: 0.5 });
-    world.spheres.push(Sphere { position: Vector3::from([0., -100.5, -1.]), radius: 100. });
 
-    while let Some(e) = window.next() {
-        let fps = fps_counter.tick() as f64;
-        dbg!(fps);
-        if e.render_args().is_some() {
-            texture.update(&mut texture_context, &canvas).unwrap();
-            window.draw_2d(&e, |c, g, device| {
-                // Update texture before rendering.
-                texture_context.encoder.flush(device);
+    let material_ground = MaterialType::Lambertian(Vector3::from([0.5,0.5,0.5]));
+    world.spheres.push(Sphere { position: Vector3::from([0., -1000., 0.]), radius: 1000., material: Material {material_type: material_ground}});
 
-                clear([0.0; 4], g);
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_material = random_0_1();
+            let center = Vector3::from([a as f64 + 0.9*random_0_1(), 0.2, b as f64 + 0.9 * random_0_1()]);
 
-                image(&texture, c.transform, g);
-            });
+            if (center - Vector3::from([4.,0.2,0.])).magnitude() > 0.9 {
+                if choose_material < 0.4 {
+                    let light = MaterialType::DiffuseLight;
+                    world.spheres.push(Sphere { position: center, radius: 0.2, material: Material {material_type: light}});
+                }
+                if choose_material < 0.8 {
+                    let color = Vector3::from([random_in_interval(Interval::from(0., 1.)), 
+                    random_in_interval(Interval::from(0., 1.)), random_in_interval(Interval::from(0., 1.))]);
+                    let sphere_material = MaterialType::Lambertian(color);
+                    world.spheres.push(Sphere { position: center, radius: 0.2, material: Material {material_type: sphere_material}});
+                }
+                else if choose_material < 0.95 {
+                    let color = Vector3::from([random_in_interval(Interval::from(0.5, 1.)), 
+                    random_in_interval(Interval::from(0., 1.)), random_in_interval(Interval::from(0., 1.))]);
+                    let fuzz = random_in_interval(Interval::from(0., 0.5));
+                    let sphere_material = MaterialType::Metal(color, fuzz);
+                    world.spheres.push(Sphere { position: center, radius: 0.2, material: Material {material_type: sphere_material}});
+                }
+                else {
+                    let sphere_material = MaterialType::Dielectric(1.5);
+                    world.spheres.push(Sphere { position: center, radius: 0.2, material: Material {material_type: sphere_material}});
+                }
+            }
         }
-
-        canvas = im::ImageBuffer::new(WINDOW_WIDTH, WINDOW_HEIGHT);
-        render(&mut canvas, camera_position, world.clone());
     }
+
+    let material1 = MaterialType::Dielectric(1.5);
+    world.spheres.push(Sphere { position: Vector3::from([0.,1.,0.]), radius: 1., material: Material {material_type: material1}});
+
+    let material2 = MaterialType::Lambertian(Vector3::from([0.4,0.2,0.1]));
+    world.spheres.push(Sphere { position: Vector3::from([-4.,1.,0.]), radius: 1., material: Material {material_type: material2}});
+
+    let material3 = MaterialType::Metal(Vector3::from([0.7,0.6,0.5]), 0.0);
+    world.spheres.push(Sphere { position: Vector3::from([4.,1.,0.]), radius: 1., material: Material {material_type: material3}});
+
+    let light = MaterialType::DiffuseLight;
+    world.spheres.push(Sphere { position: Vector3::from([0.,10.,0.]), radius: 5., material: Material {material_type: light}});
+
+
+   render(&mut canvas, world.clone(), 100, 25);
+   canvas.save("render.png").unwrap();
 }
 
